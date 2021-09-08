7c4fe6580b1474f6223f009f6f56938962bddfe3
ABIMBOLA, Idunnuoluwa <72974286+thecoder-co@users.noreply.github.com>
2021-09-08 13:13:56 +0100

    ...

--- main.py
+++ main.py
@@ -1,30 +1,15 @@
 from fastapi import FastAPI
 import unscramble as unc
 import time
-import  requests
 app = FastAPI()
 
 @app.get("/")
-async def root():
+def root():
     return {"details": "UnscrambleAPI...   baseurl/unscramble/<word>/<length>"}
 
 @app.get("/unscramble/{word}/{length}")
-async def unscramble_len(length: int,word):
+def unscramble_len(length: int,word):
     start = time.time()
     words = unc.main(length, word)
     end = time.time()
-    timed = end - start
-    return {"words": words, "time": timed}
-
-#@unscrmamble.get("/search/{word}")
-#def search(word):
-#    start = time.time()
-#    app_id = '9d7691cf1c35eafb5194dbb6a49b1932ed0f19de'
-#    app_key = '9d7691cf1c35eafb5194dbb6a49b1932ed0f19de'
-#    language = 'en-gb'
-#    url = 'https://od-api.oxforddictionaries.com/api/v2/entries/'  + language + '/'  + word.lower()
-#    r = requests.get(url, headers = {'app_id' : app_id, 'app_key' : app_key})
-#    end = time.time()
-#    timed = end - start
-#    return r.json()
-
+    return {"words": words, "time": end - start}
--- unscramble.py
+++ unscramble.py
@@ -1,19 +1,9 @@
-def main(a, letters):
-    foundWords = []
-    eng = open('words_alpha.txt', 'r') 
-    file_content = eng.read()  
-    word_list = file_content.split()   
-    for i in word_list: 
-        if len(i) <= a and isin(i, letters):
-            foundWords.append(i)
-    return foundWords
-
 def isin(lily, ylil):
-    for i in lily: 
-        if i not in ylil or lily.count(i) > ylil.count(i):
-            return False
-    #all([False for i in lily if i not in ylil or lily.count(i) > ylil.count(i)])
-    return all
-
-if __name__ == '__main__':
-    print(main(7, 'example'))
+    count = 0
+    while count < len(lily):
+        i = lily[count]
+        if i not in ylil or lily.count(i) > ylil.count(i): return False
+        count += 1
+    return True
+main = lambda a, letters: (i for i in open('words_alpha.txt', 'r').read().split() if len(i) <= a and isin(i, letters))
+if __name__ == '__main__': print(main(7, 'example'))
