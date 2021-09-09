ebd4f2936f249e19e00b44e9924a946845f84a69
ABIMBOLA, Idunnuoluwa <72974286+thecoder-co@users.noreply.github.com>
2021-09-09 07:04:05 +0100

    optional args

--- main.py
+++ main.py
@@ -1,15 +1,34 @@
 from fastapi import FastAPI
 import unscramble as unc
 import time
+from typing import Optional
+from fastapi.responses import HTMLResponse
 app = FastAPI()
 
-@app.get("/")
+@app.get("/", response_class=HTMLResponse)
 def root():
-    return {"details": "UnscrambleAPI...   baseurl/unscramble/<word>/<length>"}
+    return """
+    <html>
+        <head>
+            <title>Unscramble API</title>
+        </head>
+        <body>
+            <h1>API to get word anagrams</h1>
+            <p>baseurl/unscramble/{word}</p>
+            <p>optional arguments: length... defines maximum length of returned words</p>
+            <p>baseurl/unscramble/{word}?length={length}</p>
+        </body>
+    </html>
+    """
 
-@app.get("/unscramble/{word}/{length}")
-def unscramble_len(length: int,word):
-    start = time.time()
-    words = unc.main(length, word)
-    end = time.time()
-    return {"words": words, "time": end - start}
+@app.get("/unscramble/{word}")
+def unscramble(word: str, length: Optional[int] = None):
+    if length:
+        start = time.time()
+        words = unc.main(length, word)
+        end = time.time()
+    else:
+        start = time.time()
+        words = unc.main(len(word), word)
+        end = time.time()
+    return {"string": word, "unscrambled_words": words, "time": end - start}
--- unscramble.py
+++ unscramble.py
@@ -1,9 +1,9 @@
 def isin(lily, ylil):
-    count = 0
-    while count < len(lily):
-        i = lily[count]
+    counter = 0
+    while counter < len(lily):
+        i = lily[counter]
         if i not in ylil or lily.count(i) > ylil.count(i): return False
-        count += 1
+        counter += 1
     return True
 main = lambda a, letters: (i for i in open('words_alpha.txt', 'r').read().split() if len(i) <= a and isin(i, letters))
 if __name__ == '__main__': print(main(7, 'example'))
